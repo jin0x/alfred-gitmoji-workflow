c47700321acd91bbe4026a83c557277791759ff6
John Leskas <john.leskas@gmail.com>
2021-07-02 15:31:17 +0300

    :sparkles: Change constants

--- src/constants.py
+++ src/constants.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding=utf-8 -*-
 
-GITMOJIS_WEBSITE = 'https://gitmoji.carloscuesta.me/'
-GITMOJIS_JSON_URL = 'https://raw.githubusercontent.com/carloscuesta/gitmoji/master/src/data/gitmojis.json'
+GITMOJIS_WEBSITE = 'https://gitmoji.dev/'
+GITMOJIS_JSON_URL = 'https://raw.githubusercontent.com/jin0x/alfred-gitmoji-workflow/master/data/gitmojis.json'
 CACHE_TIME = 259200  # 3 days
 COPY_TYPE_ENV = 'GITMOJI_COPY_TYPE'
 WORKFLOW_UPDATE_SETTINGS = {
-    'github_slug': 'ai0/alfred-gitmoji-workflow'
+    'github_slug': 'jin0x/alfred-gitmoji-workflow'
 }
-GITHUB_ISSUE_URL = 'https://github.com/ai0/alfred-gitmoji-workflow/issues'
+GITHUB_ISSUE_URL = 'https://github.com/jin0x/alfred-gitmoji-workflow/issues'
